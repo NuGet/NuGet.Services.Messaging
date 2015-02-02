76d218195e8d868a54dbf97600121a472fd8b89e
johnataylor <johtaylo@microsoft.com>
2015-02-02 10:19:33 -0800

    added web service skeleton

---
+++ src/NuGet.Services.Messaging/ServiceImpl.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Owin;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace NuGet.Services.Messaging
+{
+    // authorization looks like this:
+
+    //  Claim scopeClaim = ClaimsPrincipal.Current.FindFirst("http://schemas.microsoft.com/identity/claims/scope");
+    //  bool authorized = (scopeClaim != null && scopeClaim.Value == "user_impersonation");
+
+    // and the AAD user id ...
+
+    //  Claim userClaim = ClaimsPrincipal.Current.FindFirst("http://schemas.microsoft.com/identity/claims/objectidentifier");
+    //  string userId = (userClaim != null) ? userClaim.Value : string.Empty;
+
+    public static class ServiceImpl
+    {
+        public static async Task ContactOwners(IOwinContext context)
+        {
+            //TODO: add authorization
+
+            //TODO: grab data from context and spooll into folder/storage
+
+            await context.Response.WriteAsync("ContactOwners OK");
+            context.Response.StatusCode = (int)HttpStatusCode.OK;
+        }
+
+        public static async Task ReportAbuse(IOwinContext context)
+        {
+            //TODO: add authorization
+
+            //TODO: grab data from context and spooll into folder/storage
+
+            await context.Response.WriteAsync("ReportAbuse OK");
+            context.Response.StatusCode = (int)HttpStatusCode.OK;
+        }
+
+        public static async Task ContactSupport(IOwinContext context)
+        {
+            //TODO: add authorization
+
+            //TODO: grab data from context and spooll into folder/storage
+
+            await context.Response.WriteAsync("ContactSupport OK");
+            context.Response.StatusCode = (int)HttpStatusCode.OK;
+        }
+    }
+}
---
+++ src/NuGet.Services.Messaging/Startup.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.Owin;
+using Microsoft.Owin.Security.ActiveDirectory;
+using Owin;
+using System.Configuration;
+using System.Net;
+using System.Threading.Tasks;
+
+[assembly: OwinStartup(typeof(NuGet.Services.Messaging.Startup))]
+
+namespace NuGet.Services.Messaging
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseErrorPage();
+
+            app.UseWindowsAzureActiveDirectoryBearerAuthentication(
+                new WindowsAzureActiveDirectoryBearerAuthenticationOptions
+                {
+                    Audience = ConfigurationManager.AppSettings["ida:Audience"],
+                    Tenant = ConfigurationManager.AppSettings["ida:Tenant"]
+                });
+
+            app.Run(Invoke);
+        }
+
+        async Task Invoke(IOwinContext context)
+        {
+            switch (context.Request.Method)
+            {
+                case "GET":
+                    await InvokeGET(context);
+                    break;
+                case "POST":
+                    await InvokePOST(context);
+                    break;
+                default:
+                    await context.Response.WriteAsync("NotFound");
+                    context.Response.StatusCode = (int)HttpStatusCode.NotFound;
+                    break;
+            }
+        }
+
+        async Task InvokeGET(IOwinContext context)
+        {
+            switch (context.Request.Path.Value)
+            {
+                case "/":
+                    {
+                        await context.Response.WriteAsync("OK");
+                        context.Response.StatusCode = (int)HttpStatusCode.OK;
+                        break;
+                    }
+                default:
+                    {
+                        await context.Response.WriteAsync("NotFound");
+                        context.Response.StatusCode = (int)HttpStatusCode.NotFound;
+                        break;
+                    }
+            }
+        }
+
+        async Task InvokePOST(IOwinContext context)
+        {
+            switch (context.Request.Path.Value)
+            {
+                case "/contactowners":
+                    {
+                        await ServiceImpl.ContactOwners(context);
+                        break;
+                    }
+                case "/reportabuse":
+                    {
+                        await ServiceImpl.ReportAbuse(context);
+                        break;
+                    }
+                case "/contactsupport":
+                    {
+                        await ServiceImpl.ContactSupport(context);
+                        break;
+                    }
+                default:
+                    {
+                        await context.Response.WriteAsync("NotFound");
+                        context.Response.StatusCode = (int)HttpStatusCode.NotFound;
+                        break;
+                    }
+            }
+        }
+    }
+}
