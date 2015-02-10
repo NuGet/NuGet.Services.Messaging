6117be69d7a5fc532bd183e6e56631446d80046f
Rebecca Roenitz <rebro@microsoft.com>
2015-02-10 09:36:06 -0800

    Outline of tests for GET and POST. Not all run successfully.

---
+++ MessagingServiceTests/MessagingServiceGETTests.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NuGet.Services.Messaging;
+using System.Net;
+using Newtonsoft.Json.Linq;
+
+namespace MessagingServiceTests
+{
+    
+
+    [TestClass]
+    public class ReportAbuseReasonsTests
+    {
+        
+        [TestMethod]
+        public void TestReportAbuseReasons_NuGet()
+        {
+            WebRequest request = (HttpWebRequest) WebRequest.Create("http://localhost:11717/reasons/reportAbuse/NuGet");
+            request.Method = WebRequestMethods.Http.Get;
+            WebResponse response = request.GetResponse();
+
+            Assert.AreEqual(((HttpWebResponse)response).StatusCode, HttpStatusCode.OK);
+            Assert.AreEqual(((HttpWebResponse)response).ContentType, "application/json");
+
+            Stream responseStream = response.GetResponseStream();
+            StreamReader reader = new StreamReader(responseStream);
+            String bodyContent = reader.ReadToEnd();
+            JObject root = JObject.Parse(bodyContent);
+
+            JArray reasons = root.Value<JArray>("reasons");
+
+            Assert.AreEqual(reasons[0], "The package owner is fraudulently claiming authorship");
+            Assert.AreEqual(reasons[1], "The package violates a license I own");
+            Assert.AreEqual(reasons[2], "The package contains malicious code");
+            Assert.AreEqual(reasons[3], "The package has a bug/failed to install");
+            Assert.AreEqual(reasons[4], "Other");
+            
+        }
+
+        [TestMethod]
+        public void TestReportAbuseReasons_PowerShellGallery()
+        {
+            WebRequest request = (HttpWebRequest)WebRequest.Create("http://localhost:11717/reasons/reportAbuse/PowerShellGallery");
+            request.Method = WebRequestMethods.Http.Get;
+            WebResponse response = request.GetResponse();
+
+            Assert.AreEqual(((HttpWebResponse)response).StatusCode, HttpStatusCode.OK);
+            Assert.AreEqual(((HttpWebResponse)response).ContentType, "application/json");
+
+            Stream responseStream = response.GetResponseStream();
+            StreamReader reader = new StreamReader(responseStream);
+            String bodyContent = reader.ReadToEnd();
+            JObject root = JObject.Parse(bodyContent);
+
+            JArray reasons = root.Value<JArray>("reasons");
+
+            Assert.AreEqual(reasons[0], "The module owner is fraudulently claiming authorship");
+            Assert.AreEqual(reasons[1], "The module violates a license I own");
+            Assert.AreEqual(reasons[2], "The module contains malicious code");
+            Assert.AreEqual(reasons[3], "The module has a bug/failed to install");
+            Assert.AreEqual(reasons[4], "Other");
+        }
+
+        [TestMethod]
+        public void TestReportAbuseReasons_InvalidBrand()
+        {
+            Startup s = new Startup();
+            PrivateObject pr = new PrivateObject(s);
+
+            //pr.Invoke()
+
+            WebRequest request = (HttpWebRequest)WebRequest.Create("http://localhost:11717/reasons/reportAbuse/SomeBrand");
+            request.Method = WebRequestMethods.Http.Get;
+            WebResponse response = request.GetResponse();
+
+            Assert.AreEqual(((HttpWebResponse)response).StatusCode, HttpStatusCode.NotFound);
+        }
+
+        [TestMethod]
+        public void TestReportAbuseReasons_NoBrand()
+        {
+            WebRequest request = (HttpWebRequest)WebRequest.Create("http://localhost:11717/reasons/reportAbuse");
+            request.Method = WebRequestMethods.Http.Get;
+            WebResponse response = request.GetResponse();
+
+            Assert.AreEqual(((HttpWebResponse)response).StatusCode, HttpStatusCode.NotFound);
+        }
+
+    }
+
+
+    [TestClass]
+    public class ContactSupportReasonsTests
+    {
+        
+        [TestMethod]
+        public void TestContactSupportReasons_NuGet()
+        {
+            WebRequest request = (HttpWebRequest)WebRequest.Create("http://localhost:11717/reasons/contactSupport/NuGet");
+            request.Method = WebRequestMethods.Http.Get;
+            WebResponse response = request.GetResponse();
+
+            Assert.AreEqual(((HttpWebResponse)response).StatusCode, HttpStatusCode.OK);
+            Assert.AreEqual(((HttpWebResponse)response).ContentType, "application/json");
+
+            Stream responseStream = response.GetResponseStream();
+            StreamReader reader = new StreamReader(responseStream);
+            String bodyContent = reader.ReadToEnd();
+            JObject root = JObject.Parse(bodyContent);
+
+            JArray reasons = root.Value<JArray>("reasons");
+
+            Assert.AreEqual(reasons[0], "The package contains private/confidential data");
+            Assert.AreEqual(reasons[1], "The package was published as the wrong version");
+            Assert.AreEqual(reasons[2], "The package was not intended to be published publically on this gallery");
+            Assert.AreEqual(reasons[3], "The package contains malicious code");
+            Assert.AreEqual(reasons[4], "Other");
+
+        }
+
+        [TestMethod]
+        public void TestContactSupportReasons_PowerShellGallery()
+        {
+            WebRequest request = (HttpWebRequest)WebRequest.Create("http://localhost:11717/reasons/contactSupport/PowerShellGallery");
+            request.Method = WebRequestMethods.Http.Get;
+            WebResponse response = request.GetResponse();
+
+            Assert.AreEqual(((HttpWebResponse)response).StatusCode, HttpStatusCode.OK);
+            Assert.AreEqual(((HttpWebResponse)response).ContentType, "application/json");
+
+            Stream responseStream = response.GetResponseStream();
+            StreamReader reader = new StreamReader(responseStream);
+            String bodyContent = reader.ReadToEnd();
+            JObject root = JObject.Parse(bodyContent);
+
+            JArray reasons = root.Value<JArray>("reasons");
+
+            Assert.AreEqual(reasons[0], "The module contains private/confidential data");
+            Assert.AreEqual(reasons[1], "The module was published as the wrong version");
+            Assert.AreEqual(reasons[2], "The module was not intended to be published publically on this gallery");
+            Assert.AreEqual(reasons[3], "The module contains malicious code");
+            Assert.AreEqual(reasons[4], "Other");
+        }
+
+        [TestMethod]
+        public void TestContactSupportReasons_InvalidBrand()
+        {
+            WebRequest request = (HttpWebRequest)WebRequest.Create("http://localhost:11717/reasons/contactSupport/SomeBrand");
+            request.Method = WebRequestMethods.Http.Get;
+            WebResponse response = request.GetResponse();
+
+            Assert.AreEqual(((HttpWebResponse)response).StatusCode, HttpStatusCode.NotFound);
+        }
+
+        [TestMethod]
+        public void TestContactSupportReasons_NoBrand()
+        {
+            WebRequest request = (HttpWebRequest)WebRequest.Create("http://localhost:11717/reasons/contactSupport");
+            request.Method = WebRequestMethods.Http.Get;
+            WebResponse response = request.GetResponse();
+
+            Assert.AreEqual(((HttpWebResponse)response).StatusCode, HttpStatusCode.NotFound);
+        }
+
+
+    }
+
+
+
+}
---
+++ MessagingServiceTests/MessagingServicePOSTTests.cs
@@ -0,0 +1,615 @@
+﻿using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Net;
+
+namespace MessagingServiceTests
+{
+    /// <summary>
+    /// Summary description for MessagingServicePOSTTests
+    /// </summary>
+    [TestClass]
+    public class ContactOwnersTests
+    {
+        private const string TestJSONPath = "ContactOwners.json";
+        private const string TestJSONPath_InsufficientParameters = "ContactOwners_InsufficientParams.json";
+        private const string TestJSONPath_ExtraParameters = "ContactOwners_ExtraParams.json";
+        private const string TestJSONPath_InvalidParameters = "ContactOwners_InvalidParams.json";
+
+
+        [TestMethod]
+        public void TestContactOwners()
+        {
+            // 1) Send request (containing json parameters)
+
+            WebRequest request = (HttpWebRequest)WebRequest.Create("http://localhost:11717/contactOwners");
+            request.Method = WebRequestMethods.Http.Post;
+            request.ContentType = "application/json";
+
+            // file content as a string
+            string postData = File.ReadAllText(TestJSONPath);
+            Stream streamWriter = request.GetRequestStream();
+            WebResponse response = request.GetResponse();
+
+            // CHECK:  status code is OK
+            Assert.AreEqual(((HttpWebResponse)response).StatusCode, HttpStatusCode.OK);
+            
+
+
+
+            // Get email JSON from storage
+            // parse JSON
+            // CHECK:  email to 
+            // CHECK:  email from
+            // CHECK:  email cc
+            // CHECK:  email subject is correct
+            // CHECK:  email body is formatted correctly
+
+        }
+
+        [TestMethod]
+        public void TestContactOwners_InsufficientParameters()
+        {
+            // 1) Send request (containing json parameters)
+
+            WebRequest request = (HttpWebRequest)WebRequest.Create("http://localhost:11717/contactOwners");
+            request.Method = WebRequestMethods.Http.Post;
+            request.ContentType = "application/json";
+
+            // file content as a string
+            string postData = File.ReadAllText(TestJSONPath_InsufficientParameters);
+
+            //byte[] bytePostData = Encoding.
+            Stream streamWriter = request.GetRequestStream();
+            //streamWriter.Write(postData, 0, postData.Length);
+
+
+
+
+            WebResponse response = request.GetResponse();
+
+            // CHECK:  status code is not OK (something else)
+            Assert.AreNotEqual(((HttpWebResponse)response).StatusCode, HttpStatusCode.OK);
+
+        }
+
+        [TestMethod]
+        public void TestContactOwners_ExtraParameters()
+        {
+            
+
+        }
+
+        [TestMethod]
+        public void TestContactOwners_InvalidParameters()
+        {
+            
+        }
+
+        [TestMethod]
+        public void TestContactOwners_AADConnectionFailed()
+        {
+            // Generate AAD connection failure (change connection strings so connection cannot be made?)
+
+            // Check status is ServiceUnavailable
+
+            //HttpStatusCode.ServiceUnavailable
+        }
+
+        [TestMethod]
+        public void TestContactOwners_StorageConnectionFailed()
+        {
+            // Generate Storage connection failure (change connection strings so connection cannot be made?)
+
+            // Check status is ServiceUnavailable
+
+            //HttpStatusCode.ServiceUnavailable
+        }
+
+    }
+
+
+    [TestClass]
+    public class ReportAbuseTests
+    {
+        private const string TestJSONPath = "ReportAbuse.json";
+        private const string TestJSONPath_InsufficientParameters = "ReportAbuse_InsufficientParams.json";
+        private const string TestJSONPath_ExtraParameters = "ReportAbuse_ExtraParams.json";
+        private const string TestJSONPath_InvalidParameters = "ReportAbuse_InvalidParams.json";
+
+
+        [TestMethod]
+        public void TestReportAbuse()
+        {
+            // 1) Send request (containing json parameters)
+
+            WebRequest request = (HttpWebRequest)WebRequest.Create("http://localhost:11717/ReportAbuse");
+            request.Method = WebRequestMethods.Http.Post;
+            request.ContentType = "application/json";
+
+            // file content as a string
+            string postData = File.ReadAllText(TestJSONPath);
+
+            //byte[] bytePostData = Encoding.
+            Stream streamWriter = request.GetRequestStream();
+            //streamWriter.Write(postData, 0, postData.Length);
+
+
+
+
+            WebResponse response = request.GetResponse();
+
+            // CHECK:  status code is OK
+            Assert.AreEqual(((HttpWebResponse)response).StatusCode, HttpStatusCode.OK);
+
+
+
+
+            // Get email blob from storage
+
+            // CHECK:  email to 
+            // CHECK:  email from
+            // CHECK:  email subject is correct
+            // CHECK:  email body is formatted correctly
+
+        }
+
+        [TestMethod]
+        public void TestReportAbuse_InsufficientParameters()
+        {
+            // 1) Send request (containing json parameters)
+
+            WebRequest request = (HttpWebRequest)WebRequest.Create("http://localhost:11717/ReportAbuse");
+            request.Method = WebRequestMethods.Http.Post;
+            request.ContentType = "application/json";
+
+            // file content as a string
+            string postData = File.ReadAllText(TestJSONPath_InsufficientParameters);
+
+            //byte[] bytePostData = Encoding.
+            Stream streamWriter = request.GetRequestStream();
+            //streamWriter.Write(postData, 0, postData.Length);
+
+
+
+
+            WebResponse response = request.GetResponse();
+
+            // CHECK:  status code is OK
+            Assert.AreEqual(((HttpWebResponse)response).StatusCode, HttpStatusCode.OK);
+
+
+
+
+            // Get email blob from storage
+
+            // CHECK:  email to 
+            // CHECK:  email from
+            // CHECK:  email subject is correct
+            // CHECK:  email body is formatted correctly
+
+        }
+
+        [TestMethod]
+        public void TestReportAbuse_ExtraParameters()
+        {
+            // 1) Send request (containing json parameters)
+
+            WebRequest request = (HttpWebRequest)WebRequest.Create("http://localhost:11717/ReportAbuse");
+            request.Method = WebRequestMethods.Http.Post;
+            request.ContentType = "application/json";
+
+            // file content as a string
+            string postData = File.ReadAllText(TestJSONPath_ExtraParameters);
+
+            //byte[] bytePostData = Encoding.
+            Stream streamWriter = request.GetRequestStream();
+            //streamWriter.Write(postData, 0, postData.Length);
+
+
+
+
+            WebResponse response = request.GetResponse();
+
+            // CHECK:  status code is OK
+            Assert.AreEqual(((HttpWebResponse)response).StatusCode, HttpStatusCode.OK);
+
+
+
+
+            // Get email blob from storage
+
+            // CHECK:  email to 
+            // CHECK:  email from
+            // CHECK:  email subject is correct
+            // CHECK:  email body is formatted correctly
+
+        }
+
+        [TestMethod]
+        public void TestReportAbuse_InvalidParameters()
+        {
+            // 1) Send request (containing json parameters)
+
+            WebRequest request = (HttpWebRequest)WebRequest.Create("http://localhost:11717/ReportAbuse");
+            request.Method = WebRequestMethods.Http.Post;
+            request.ContentType = "application/json";
+
+            // file content as a string
+            string postData = File.ReadAllText(TestJSONPath_InvalidParameters);
+
+            //byte[] bytePostData = Encoding.
+            Stream streamWriter = request.GetRequestStream();
+            //streamWriter.Write(postData, 0, postData.Length);
+
+
+
+
+            WebResponse response = request.GetResponse();
+
+            // CHECK:  status code is OK
+            Assert.AreEqual(((HttpWebResponse)response).StatusCode, HttpStatusCode.OK);
+
+
+
+
+            // Get email blob from storage
+
+            // CHECK:  email to 
+            // CHECK:  email from
+            // CHECK:  email subject is correct
+            // CHECK:  email body is formatted correctly
+
+        }
+
+        [TestMethod]
+        public void TestReportAbuse_AADConnectionFailed()
+        {
+
+        }
+
+        [TestMethod]
+        public void TestReportAbuse_StorageConnectionFailed()
+        {
+
+        }
+
+
+    }
+
+
+    [TestClass]
+    public class ContactSupportTests
+    {
+        private const string TestJSONPath = "ContactSupport.json";
+        private const string TestJSONPath_InsufficientParameters = "ContactSupport_InsufficientParams.json";
+        private const string TestJSONPath_ExtraParameters = "ContactSupport_ExtraParams.json";
+        private const string TestJSONPath_InvalidParameters = "ContactSupport_InvalidParams.json";
+
+
+        [TestMethod]
+        public void TestContactSupport()
+        {
+            // 1) Send request (containing json parameters)
+
+            WebRequest request = (HttpWebRequest)WebRequest.Create("http://localhost:11717/ContactSupport");
+            request.Method = WebRequestMethods.Http.Post;
+            request.ContentType = "application/json";
+
+            // file content as a string
+            string postData = File.ReadAllText(TestJSONPath);
+
+            //byte[] bytePostData = Encoding.
+            Stream streamWriter = request.GetRequestStream();
+            //streamWriter.Write(postData, 0, postData.Length);
+
+
+
+
+            WebResponse response = request.GetResponse();
+
+            // CHECK:  status code is OK
+            Assert.AreEqual(((HttpWebResponse)response).StatusCode, HttpStatusCode.OK);
+
+
+
+
+            // Get email blob from storage
+
+            // CHECK:  email to 
+            // CHECK:  email from
+            // CHECK:  email subject is correct
+            // CHECK:  email body is formatted correctly
+
+        }
+
+        [TestMethod]
+        public void TestContactSupport_InsufficientParameters()
+        {
+            // 1) Send request (containing json parameters)
+
+            WebRequest request = (HttpWebRequest)WebRequest.Create("http://localhost:11717/ContactSupport");
+            request.Method = WebRequestMethods.Http.Post;
+            request.ContentType = "application/json";
+
+            // file content as a string
+            string postData = File.ReadAllText(TestJSONPath_InsufficientParameters);
+
+            //byte[] bytePostData = Encoding.
+            Stream streamWriter = request.GetRequestStream();
+            //streamWriter.Write(postData, 0, postData.Length);
+
+
+
+
+            WebResponse response = request.GetResponse();
+
+            // CHECK:  status code is OK
+            Assert.AreEqual(((HttpWebResponse)response).StatusCode, HttpStatusCode.OK);
+
+
+
+
+            // Get email blob from storage
+
+            // CHECK:  email to 
+            // CHECK:  email from
+            // CHECK:  email subject is correct
+            // CHECK:  email body is formatted correctly
+
+        }
+
+        [TestMethod]
+        public void TestContactSupport_ExtraParameters()
+        {
+            // 1) Send request (containing json parameters)
+
+            WebRequest request = (HttpWebRequest)WebRequest.Create("http://localhost:11717/ContactSupport");
+            request.Method = WebRequestMethods.Http.Post;
+            request.ContentType = "application/json";
+
+            // file content as a string
+            string postData = File.ReadAllText(TestJSONPath_ExtraParameters);
+
+            //byte[] bytePostData = Encoding.
+            Stream streamWriter = request.GetRequestStream();
+            //streamWriter.Write(postData, 0, postData.Length);
+
+
+
+
+            WebResponse response = request.GetResponse();
+
+            // CHECK:  status code is OK
+            Assert.AreEqual(((HttpWebResponse)response).StatusCode, HttpStatusCode.OK);
+
+
+
+
+            // Get email blob from storage
+
+            // CHECK:  email to 
+            // CHECK:  email from
+            // CHECK:  email subject is correct
+            // CHECK:  email body is formatted correctly
+
+        }
+
+        [TestMethod]
+        public void TestContactSupport_InvalidParameters()
+        {
+            // 1) Send request (containing json parameters)
+
+            WebRequest request = (HttpWebRequest)WebRequest.Create("http://localhost:11717/ContactSupport");
+            request.Method = WebRequestMethods.Http.Post;
+            request.ContentType = "application/json";
+
+            // file content as a string
+            string postData = File.ReadAllText(TestJSONPath_InvalidParameters);
+
+            //byte[] bytePostData = Encoding.
+            Stream streamWriter = request.GetRequestStream();
+            //streamWriter.Write(postData, 0, postData.Length);
+
+
+
+
+            WebResponse response = request.GetResponse();
+
+            // CHECK:  status code is OK
+            Assert.AreEqual(((HttpWebResponse)response).StatusCode, HttpStatusCode.OK);
+
+
+
+
+            // Get email blob from storage
+
+            // CHECK:  email to 
+            // CHECK:  email from
+            // CHECK:  email subject is correct
+            // CHECK:  email body is formatted correctly
+
+        }
+
+        [TestMethod]
+        public void TestContactSupport_AADConnectionFailed()
+        {
+
+        }
+
+        [TestMethod]
+        public void TestContactSupport_StorageConnectionFailed()
+        {
+
+        }
+    }
+
+
+    [TestClass]
+    public class InvitePackageOwnerTests
+    {
+        private const string TestJSONPath = "InvitePackageOwner.json";
+        private const string TestJSONPath_InsufficientParameters = "InvitePackageOwner_InsufficientParams.json";
+        private const string TestJSONPath_ExtraParameters = "InvitePackageOwner_ExtraParams.json";
+        private const string TestJSONPath_InvalidParameters = "InvitePackageOwner_InvalidParams.json";
+
+
+        [TestMethod]
+        public void TestInvitePackageOwner()
+        {
+            // 1) Send request (containing json parameters)
+
+            WebRequest request = (HttpWebRequest)WebRequest.Create("http://localhost:11717/InvitePackageOwner");
+            request.Method = WebRequestMethods.Http.Post;
+            request.ContentType = "application/json";
+
+            // file content as a string
+            string postData = File.ReadAllText(TestJSONPath);
+
+            //byte[] bytePostData = Encoding.
+            Stream streamWriter = request.GetRequestStream();
+            //streamWriter.Write(postData, 0, postData.Length);
+
+
+
+
+            WebResponse response = request.GetResponse();
+
+            // CHECK:  status code is OK
+            Assert.AreEqual(((HttpWebResponse)response).StatusCode, HttpStatusCode.OK);
+
+
+
+
+            // Get email blob from storage
+
+            // CHECK:  email to 
+            // CHECK:  email from
+            // CHECK:  email subject is correct
+            // CHECK:  email body is formatted correctly
+
+        }
+
+        [TestMethod]
+        public void TestInvitePackageOwner_InsufficientParameters()
+        {
+            // 1) Send request (containing json parameters)
+
+            WebRequest request = (HttpWebRequest)WebRequest.Create("http://localhost:11717/InvitePackageOwner");
+            request.Method = WebRequestMethods.Http.Post;
+            request.ContentType = "application/json";
+
+            // file content as a string
+            string postData = File.ReadAllText(TestJSONPath_InsufficientParameters);
+
+            //byte[] bytePostData = Encoding.
+            Stream streamWriter = request.GetRequestStream();
+            //streamWriter.Write(postData, 0, postData.Length);
+
+
+
+
+            WebResponse response = request.GetResponse();
+
+            // CHECK:  status code is OK
+            Assert.AreEqual(((HttpWebResponse)response).StatusCode, HttpStatusCode.OK);
+
+
+
+
+            // Get email blob from storage
+
+            // CHECK:  email to 
+            // CHECK:  email from
+            // CHECK:  email subject is correct
+            // CHECK:  email body is formatted correctly
+
+        }
+
+        [TestMethod]
+        public void TestInvitePackageOwner_ExtraParameters()
+        {
+            // 1) Send request (containing json parameters)
+
+            WebRequest request = (HttpWebRequest)WebRequest.Create("http://localhost:11717/InvitePackageOwner");
+            request.Method = WebRequestMethods.Http.Post;
+            request.ContentType = "application/json";
+
+            // file content as a string
+            string postData = File.ReadAllText(TestJSONPath_ExtraParameters);
+
+            //byte[] bytePostData = Encoding.
+            Stream streamWriter = request.GetRequestStream();
+            //streamWriter.Write(postData, 0, postData.Length);
+
+
+
+
+            WebResponse response = request.GetResponse();
+
+            // CHECK:  status code is OK
+            Assert.AreEqual(((HttpWebResponse)response).StatusCode, HttpStatusCode.OK);
+
+
+
+
+            // Get email blob from storage
+
+            // CHECK:  email to 
+            // CHECK:  email from
+            // CHECK:  email subject is correct
+            // CHECK:  email body is formatted correctly
+
+        }
+
+        [TestMethod]
+        public void TestInvitePackageOwner_InvalidParameters()
+        {
+            // 1) Send request (containing json parameters)
+
+            WebRequest request = (HttpWebRequest)WebRequest.Create("http://localhost:11717/InvitePackageOwner");
+            request.Method = WebRequestMethods.Http.Post;
+            request.ContentType = "application/json";
+
+            // file content as a string
+            string postData = File.ReadAllText(TestJSONPath_InvalidParameters);
+
+            //byte[] bytePostData = Encoding.
+            Stream streamWriter = request.GetRequestStream();
+            //streamWriter.Write(postData, 0, postData.Length);
+
+
+
+
+            WebResponse response = request.GetResponse();
+
+            // CHECK:  status code is OK
+            Assert.AreEqual(((HttpWebResponse)response).StatusCode, HttpStatusCode.OK);
+
+
+
+
+            // Get email blob from storage
+
+            // CHECK:  email to 
+            // CHECK:  email from
+            // CHECK:  email subject is correct
+            // CHECK:  email body is formatted correctly
+
+        }
+
+        [TestMethod]
+        public void TestInvitePackageOwner_AADConnectionFailed()
+        {
+
+        }
+
+        [TestMethod]
+        public void TestInvitePackageOwner_StorageConnectionFailed()
+        {
+
+        }
+    }
+
+
+
+}
