a0a77de94ddfca230d00747c7151ec3cfefc5fb2
Rebecca Roenitz <rebro@microsoft.com>
2015-02-18 11:18:15 -0800

    Added Resources and ResourceManager class outlines.  Implementation to come later.

---
+++ src/NuGet.Services.Messaging/Resources/AADResource.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Threading.Tasks;
+using Microsoft.Azure.ActiveDirectory.GraphClient;
+using Microsoft.Azure.ActiveDirectory.GraphClient.Extensions;
+using Microsoft.IdentityModel.Clients.ActiveDirectory;
+
+namespace NuGet.Services.Messaging
+{
+
+    /*
+     * Used for all interaction with AAD to obtain information about entities or users.
+     */
+    public class AADResource : IResource
+    {
+
+        private ActiveDirectoryClient _client;
+
+        public AADResource() { }
+
+
+        public async Task Initialize(string aadInstance, string tenant, string clientId, string appKey, string graphResourceId) 
+        {
+            // Taken from NuGet.Services.Publish.ServiceHelpers.cs
+
+            string authority = string.Format(aadInstance, tenant);
+            AuthenticationContext authContext = new AuthenticationContext(authority);
+
+            ClientCredential clientCredential = new ClientCredential(clientId, appKey);
+            AuthenticationResult result = await authContext.AcquireTokenAsync(graphResourceId, clientCredential);
+
+            string accessToken = result.AccessToken;
+
+            Uri serviceRoot = new Uri(new Uri(graphResourceId), tenant);
+
+            _client = new ActiveDirectoryClient(serviceRoot, () => { return Task.FromResult(accessToken); });
+
+        }
+
+
+
+        public Task<string> GetEmail(string username)
+        {
+            
+            //IUser user = await _client.Users.GetByObjectId(username).ExecuteAsync();
+            //string emailAddress = user.Mail;  // assuming email is stored in mail
+            //return emailAddress;
+            
+            throw new NotImplementedException();
+        }
+
+        public Task<string[]> GetOwnersEmails(string packageID)
+        {
+            // get owners
+
+            // for each owner, get email address
+            
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> IsContactAllowed(string packageID)
+        {
+
+
+            throw new NotImplementedException();
+        }
+
+
+    }
+}
---
+++ src/NuGet.Services.Messaging/Resources/SQLResource.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Threading.Tasks;
+
+
+
+namespace NuGet.Services.Messaging.Resources
+{
+
+    /*
+     * Used for all interaction with SQL DB to obtain information about entities and users.
+     */
+    public class SQLResource : IResource
+    {
+        // private SQLConnection _connector;
+
+
+        public SQLResource() { }
+
+
+        public void Initialize()
+        {
+            // create the connector using some connection info
+        }
+
+
+        public Task<string> GetEmail(string username)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string[]> GetOwnersEmails(string packageID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> IsContactAllowed(string packageID)
+        {
+            throw new NotImplementedException();
+        }
+
+
+    }
+}
